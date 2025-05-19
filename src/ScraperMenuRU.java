b6419267793c4918212d15b6c0f48bdee6ce9d50
Stefany <stefanycn.dev@gmail.com>
2025-05-19 10:26:53 -0300

    Class web scraper to get RU's menu. It only prints on the terminal the information

---
+++ src/ScraperMenuRU.java
@@ -0,0 +1,63 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class ScraperMenuRU {
+    public static void main(String[] args) {
+        String url = "https://www.ufrgs.br/prae/cardapio-ru/";
+
+        try {
+            // gets the HTML content of the page
+            Document doc = Jsoup.connect(url).get();
+
+            // select all table elements from the page
+            Elements tables = doc.select("table");
+
+            int countRU = 0;
+            int countTable = 0;
+            // iterate through all tables
+            for (Element table : tables) {
+
+                countTable++;
+                if (countTable == 5) {
+                    countRU++;
+                    System.out.println();
+                    System.out.println("============ RU0" + countRU + " ============");
+                    System.out.println("+++++++++ ALMOÇO +++++++++++");
+                    countTable++;
+                } else {
+                    if (countTable % 2 == 0) {
+                        System.out.println();
+                        System.out.println("+++++++++ JANTAR +++++++++++");
+                    } else {
+                        countRU++;
+                        System.out.println();
+                        System.out.println("============ RU0" + countRU + " ============");
+                        System.out.println("+++++++++ ALMOÇO +++++++++++");
+                    }
+                }
+
+                // gets all rows in the table
+                Elements rows = table.select("tr");
+
+                // iterate through all rows
+                for (Element row : rows) {
+                    // gets the columns (td or th) in the row (days of the week)
+                    Elements cols = row.select("td, th");
+
+                    // iterate through all columns and print their text content
+                    for (Element col : cols) {
+                        System.out.print(col.text() + " | ");
+                    }
+                    System.out.println();
+                }
+
+            }
+        } catch (IOException e) {
+            System.out.println("Error fetching the website: " + e.getMessage());
+        }
+    }
+}
