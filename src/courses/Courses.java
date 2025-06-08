f27807c7d1f91e217c00669726d44706cf964111
Eduardo Veiga <eduardo85011162@gmail.com>
2025-06-08 19:16:04 -0300

    Add Makefile to automate integration, tests, compilation and execution

---
+++ src/courses/Courses.java
@@ -0,0 +1,17 @@
+package courses;
+
+import java.util.ArrayList;
+import java.util.List;
+import tasks.*;
+
+public class Courses {
+
+    private String code;
+    private String name;
+    private int credits;
+    private Teacher teacher;
+    private List<Schedule> schedules;
+    private List<Task> tasks;
+    private String link_moodle;
+
+}
