22f677a1bb451c869ac05d18985727220fdd9e6c
Stefany Nascimento <stefanycn.dev@gmail.com>
2025-06-09 18:31:04 -0300

    Creation of activity files for navigation and scratch of design

---
+++ src/androidApplication/app/src/main/java/br/ufrgs/inf/tcp/tcheorganiza/ui/startingpage/StartingPageActivity.java
@@ -0,0 +1,13 @@
+package br.ufrgs.inf.tcp.tcheorganiza.ui.startingpage;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StartingPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ src/androidApplication/app/src/main/java/br/ufrgs/inf/tcp/tcheorganiza/ui/tasks/NewTaskActivity.java
@@ -0,0 +1,19 @@
+package br.ufrgs.inf.tcp.tcheorganiza.ui.tasks;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import br.ufrgs.inf.tcp.tcheorganiza.R;
+
+public class NewTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
