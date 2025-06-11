ec28632edbdb6c9ba0c4ed3a9ffe59e11fadd8f3
Stefany Nascimento <stefanycn.dev@gmail.com>
2025-06-11 16:25:39 -0300

    All navigation to activities working (always starts with the welcome page)

--- src/androidApplication/app/src/main/java/br/ufrgs/inf/tcp/tcheorganiza/ui/disciplinas/AddClassActivity.java
+++ src/androidApplication/app/src/main/java/br/ufrgs/inf/tcp/tcheorganiza/ui/disciplinas/AddClassActivity.java
@@ -1,6 +1,8 @@
 package br.ufrgs.inf.tcp.tcheorganiza.ui.disciplinas;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,9 +10,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import br.ufrgs.inf.tcp.tcheorganiza.NewClassActivity;
+import br.ufrgs.inf.tcp.tcheorganiza.NewTaskActivity;
 import br.ufrgs.inf.tcp.tcheorganiza.R;
 import br.ufrgs.inf.tcp.tcheorganiza.databinding.ActivityAddClassBinding;
 import br.ufrgs.inf.tcp.tcheorganiza.databinding.ActivityNewClassBinding;
+import br.ufrgs.inf.tcp.tcheorganiza.ui.professor.AddProfessorActivity;
 
 public class AddClassActivity extends AppCompatActivity {
 
@@ -22,5 +27,12 @@ public class AddClassActivity extends AppCompatActivity {
 
         binding = ActivityAddClassBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        binding.buttonAddProf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent newAddProfIntent = new Intent(AddClassActivity.this, AddProfessorActivity.class);
+                startActivity(newAddProfIntent);
+            }
+        });
     }
 }
