0cac01dce04c606d6672ea5102e7596cc6ede5dd
Stefany Nascimento <stefanycn.dev@gmail.com>
2025-06-13 14:33:48 -0300

    New task activity design and buttons working.

--- src/androidApplication/app/src/main/java/br/ufrgs/inf/tcp/tcheorganiza/NewTaskActivity.java
+++ src/androidApplication/app/src/main/java/br/ufrgs/inf/tcp/tcheorganiza/NewTaskActivity.java
@@ -1,10 +1,18 @@
 package br.ufrgs.inf.tcp.tcheorganiza;
 
 import android.content.Intent;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.ContextThemeWrapper;
 import android.view.View;
+import android.widget.AutoCompleteTextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.core.content.ContextCompat;
 
 import br.ufrgs.inf.tcp.tcheorganiza.databinding.ActivityMainBinding;
 import br.ufrgs.inf.tcp.tcheorganiza.databinding.ActivityNewTaskBinding;
@@ -19,6 +27,27 @@ public class NewTaskActivity extends AppCompatActivity {
 
         binding = ActivityNewTaskBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        setupDesignSearchBox();
+        setButtonContinuar();
+        setButtonAdicionar();
+
+
+    }
+    private void setupDesignSearchBox(){
+        SearchView searchView = findViewById(R.id.search_view_disciplina);
+
+        AutoCompleteTextView searchText = searchView.findViewById(androidx.appcompat.R.id.search_src_text);
+        searchText.setTypeface(Typeface.SANS_SERIF);
+
+        searchText.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16);
+
+        TypedValue typedValue = new TypedValue();
+        Resources.Theme theme = getTheme();
+        theme.resolveAttribute(android.R.attr.textColorHint, typedValue, true);
+        searchText.setHintTextColor(ContextCompat.getColor(this, typedValue.resourceId));
+    }
+
+    private void setButtonContinuar(){
         binding.buttonContinuar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -26,5 +55,32 @@ public class NewTaskActivity extends AppCompatActivity {
                 startActivity(newTaskActivityIntent);
             }
         });
+    }
+    private void setButtonAdicionar(){
+        binding.buttonAddTasks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //ADICIONAR FUNÇÃO AQUI PARA PEGAR AS INFORMAÇÕES
+                clearTextFields();
+            }
+        });
+    }
+    private void clearTextFields(){
+        binding.textInputNomeAtividade.setText("");
+        binding.textInputNomeAtividade.clearFocus();
+
+
+        binding.textInputData.setText("");
+        binding.textInputData.clearFocus();
+
+
+        binding.textInputTipo.setText("");
+        binding.textInputTipo.clearFocus();
+
+        binding.searchViewDisciplina.setQuery("", false);
+        binding.searchViewDisciplina.clearFocus();
+
+        binding.textInputDescricao.setText("");
+        binding.textInputDescricao.clearFocus();
     }
 }
