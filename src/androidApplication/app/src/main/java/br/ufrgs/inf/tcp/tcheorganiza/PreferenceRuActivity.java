107a7b5b566fdc08d9c2be75fe0acaa110d4d4b9
Stefany Nascimento <stefanycn.dev@gmail.com>
2025-06-13 15:53:24 -0300

    Setting up dropdown for RU preference choice and refactoring methods

--- src/androidApplication/app/src/main/java/br/ufrgs/inf/tcp/tcheorganiza/PreferenceRuActivity.java
+++ src/androidApplication/app/src/main/java/br/ufrgs/inf/tcp/tcheorganiza/PreferenceRuActivity.java
@@ -3,25 +3,41 @@ package br.ufrgs.inf.tcp.tcheorganiza;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-import br.ufrgs.inf.tcp.tcheorganiza.databinding.ActivityNewClassBinding;
 import br.ufrgs.inf.tcp.tcheorganiza.databinding.ActivityPreferenceRuBinding;
 
 public class PreferenceRuActivity extends AppCompatActivity {
 
     private ActivityPreferenceRuBinding binding;
+
+    //Creating activity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         binding = ActivityPreferenceRuBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        setupDropdownRU();
+        setButtonConcluido();
+    }
+
+    //Setting up dropdown menu
+    private void setupDropdownRU(){
+        AutoCompleteTextView dropdown = findViewById(R.id.dropdown_menu_RU);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.dropdown_options_RU,
+                android.R.layout.simple_dropdown_item_1line
+        );
+        dropdown.setAdapter(adapter);
+    }
+
+    //Setting up button
+    private void setButtonConcluido(){
         binding.buttonConcluido.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -30,4 +46,5 @@ public class PreferenceRuActivity extends AppCompatActivity {
             }
         });
     }
+
 }
