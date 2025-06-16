c412adfd0e5277ee4e2b0010f9ec1ba59835f6e3
Stefany Nascimento <stefanycn.dev@gmail.com>
2025-06-16 19:16:37 -0300

    Adding dropdown "Disciplinas" and DatePicker

--- src/androidApplication/app/src/main/java/br/ufrgs/inf/tcp/tcheorganiza/NewTaskActivity.java
+++ src/androidApplication/app/src/main/java/br/ufrgs/inf/tcp/tcheorganiza/NewTaskActivity.java
@@ -1,12 +1,14 @@
 package br.ufrgs.inf.tcp.tcheorganiza;
 
+
+
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.content.res.Resources;
-import android.content.res.TypedArray;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.TypedValue;
-import android.view.ContextThemeWrapper;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
@@ -15,12 +17,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
 import androidx.core.content.ContextCompat;
 
-import br.ufrgs.inf.tcp.tcheorganiza.databinding.ActivityMainBinding;
+import java.util.Calendar;
+
 import br.ufrgs.inf.tcp.tcheorganiza.databinding.ActivityNewTaskBinding;
+import br.ufrgs.inf.tcp.tcheorganiza.model.courses.Course;
+import br.ufrgs.inf.tcp.tcheorganiza.model.courses.Teacher;
+import br.ufrgs.inf.tcp.tcheorganiza.persistence.TcheOrganizaPersistence;
 
 public class NewTaskActivity extends AppCompatActivity {
 
     private ActivityNewTaskBinding binding;
+    private AutoCompleteTextView courseAutoComplete;
+    private Course selectCourseInstance;
 
     //Creating activity
     @Override
@@ -30,10 +38,10 @@ public class NewTaskActivity extends AppCompatActivity {
         binding = ActivityNewTaskBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         setupDropdownTipo();
-        setupDesignSearchBox();
         setButtonContinuar();
         setButtonAdicionar();
-
+        setupDropdownDisciplinas();
+        binding.textInputData.setOnClickListener(v -> showDatePicker());
 
     }
 
@@ -46,25 +54,26 @@ public class NewTaskActivity extends AppCompatActivity {
                 android.R.layout.simple_dropdown_item_1line
         );
         dropdown.setAdapter(adapter);
+
     }
     private void resetDropdownTipo(){
+        courseAutoComplete = binding.dropdownMenuDisciplina;
         AutoCompleteTextView dropdown = findViewById(R.id.dropdown_menu_tipo);
         dropdown.setText("",false);
     }
 
-    //Search box for "Disciplinas"
-    private void setupDesignSearchBox(){
-        SearchView searchView = findViewById(R.id.search_view_disciplina);
-
-        AutoCompleteTextView searchText = searchView.findViewById(androidx.appcompat.R.id.search_src_text);
-        searchText.setTypeface(Typeface.SANS_SERIF);
-
-        searchText.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16);
-
-        TypedValue typedValue = new TypedValue();
-        Resources.Theme theme = getTheme();
-        theme.resolveAttribute(android.R.attr.textColorHint, typedValue, true);
-        searchText.setHintTextColor(ContextCompat.getColor(this, typedValue.resourceId));
+    private void setupDropdownDisciplinas(){
+        courseAutoComplete = findViewById(R.id.dropdown_menu_disciplina);
+        ArrayAdapter<Course> adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_dropdown_item_1line,
+                TcheOrganizaPersistence.getInstance().getDisciplinasList()
+        );
+        courseAutoComplete.setAdapter(adapter);
+        courseAutoComplete.setOnItemClickListener((parent, view, position, id) -> {
+            Course selectedCourse = (Course) parent.getItemAtPosition(position);
+            selectCourseInstance = selectedCourse;
+        });
     }
 
     //Setting up buttons
@@ -82,9 +91,53 @@ public class NewTaskActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //ADICIONAR FUNÇÃO AQUI PARA PEGAR AS INFORMAÇÕES
-                clearTextFields();
+                saveNewTask();
+                new AlertDialog.Builder(NewTaskActivity.this)
+                        .setMessage("Atividade adicionada com sucesso!")
+                        .setPositiveButton("OK", (dialog, which) -> {
+                            clearTextFields();})
+                        .show();
             }
         });
+    }
+    private int yearSelected;
+    private int monthSelected;
+    private int daySelected;
+    private String dateSelected;
+
+    private void showDatePicker() {
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(NewTaskActivity.this,
+                (view, selectedYear, selectedMonth, selectedDay) -> {
+                    yearSelected = selectedYear;
+                    monthSelected = selectedMonth;
+                    daySelected = selectedDay;
+                    dateSelected = String.format("%02d/%02d/%04d", daySelected, monthSelected + 1, yearSelected);
+                    binding.textInputData.setText(dateSelected);
+                }, year, month, day);
+        datePickerDialog.show();
+
+
+    }
+    private String getTaskName(){
+        return binding.textInputNomeAtividade.getText().toString().trim();
+    }
+
+    private String getTaskType(){
+       return binding.dropdownMenuTipo.getText().toString().trim();
+    }
+    private String getTaskDescription(){
+        return binding.textInputDescricao.getText().toString();
+    }
+
+    private void saveNewTask() {
+        getTaskName();
+        getTaskType();
+        getTaskDescription();
     }
 
     //Clearing fields for next Task to be add
@@ -92,16 +145,15 @@ public class NewTaskActivity extends AppCompatActivity {
         binding.textInputNomeAtividade.setText("");
         binding.textInputNomeAtividade.clearFocus();
 
-
         binding.textInputData.setText("");
         binding.textInputData.clearFocus();
 
-
         resetDropdownTipo();
         binding.textInputDropdownTipo.clearFocus();
+        
+        binding.dropdownMenuDisciplina.setText("");
+        binding.dropdownMenuDisciplina.clearFocus();
 
-        binding.searchViewDisciplina.setQuery("", false);
-        binding.searchViewDisciplina.clearFocus();
 
         binding.textInputDescricao.setText("");
         binding.textInputDescricao.clearFocus();
