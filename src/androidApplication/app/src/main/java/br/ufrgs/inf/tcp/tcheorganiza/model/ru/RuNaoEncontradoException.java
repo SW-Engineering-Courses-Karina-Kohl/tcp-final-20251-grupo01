cdc96237a54b086285eb7b6569d176538f2e08d9
Aline Fraga da Silva <109742288+alinefragadasilvaa@users.noreply.github.com>
2025-06-19 14:26:56 -0300

    Update package of RuNaoEncontradoException.java

--- src/androidApplication/app/src/main/java/br/ufrgs/inf/tcp/tcheorganiza/model/ru/RuNaoEncontradoException.java
+++ src/androidApplication/app/src/main/java/br/ufrgs/inf/tcp/tcheorganiza/model/ru/RuNaoEncontradoException.java
@@ -1,3 +1,5 @@
+package br.ufrgs.inf.tcp.tcheorganiza.model.ru;
+
 public class RuNaoEncontradoException extends RuntimeException {
     public RuNaoEncontradoException(String mensagem) {
         super(mensagem);
