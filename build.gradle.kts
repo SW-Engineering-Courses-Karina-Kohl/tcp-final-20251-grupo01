06b241dede751641bbf51fe28f76849171917319
Adriel de Souza <ds.adriel@gmail.com>
2025-07-06 19:36:25 -0300

    fix: correct syntax for applying SonarQube plugin in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
-    id "org.sonarqube" version "6.2.0.5505"
+    id("org.sonarqube") version "6.2.0.5505"
 }
 
 sonar {
